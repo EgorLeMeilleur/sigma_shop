1f44de0b5eeb14650ff7ede1bd6e64763fb6a552
egorlemeilleur <egorpokumin@gmail.com>
2023-08-02 22:10:42 +0500

    отправка на почту информации о заказе

--- purchase/views.py
+++ purchase/views.py
@@ -4,7 +4,10 @@ from django.shortcuts import render, redirect, get_object_or_404
 # Create your views here.
 from products.models import Product
 from purchase.models import Purchase
+from sigma_shop.settings import EMAIL_HOST_USER
 from wishes.models import Wishes
+from django.core.mail import send_mail
+from django.core.mail import EmailMessage
 
 
 def purchase(request):
@@ -12,8 +15,17 @@ def purchase(request):
         user_id = request.user
         wishes = Wishes.objects.filter(user=user_id)
         if wishes:
+            subject = 'Заказ от ' + user_id.username + '. Телефон: ' + str(user_id.user_profile.phone_number) + '. Электронная почта: ' + str(user_id.user_profile.email) + '. '
+            message = ''
             for wish in wishes:
                 new_obj = Purchase(user=user_id, product=wish.product, quantity=wish.quantity)
+                message += 'Продукт ' + wish.product.name + ' в количестве ' + str(wish.quantity) + ' шт. Цена: ' + str(wish.product.get_discounted_price()) + ' р.\n'
                 new_obj.save()
                 wish.delete()
+            email = EmailMessage(
+                subject,
+                message,
+                EMAIL_HOST_USER,
+                ['sigmamaleshop@yandex.ru'])
+            email.send()
     return redirect('personal_cabinet')
